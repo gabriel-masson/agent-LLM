f8997cfb8c7a6fc6b64286eb47bb7dd90e45f638
gabriel-masson <60262748+gabriel-masson@users.noreply.github.com>
2026-09-11 16:18:17 -0300

    .

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import requests
-from dotenv import load_dotenv
 
 from langchain_ollama import ChatOllama
 from langchain.agents import create_agent
 from langchain.tools import tool
 
 
-@tool('get_weather', return_direct=False, description='Get the current weather for a given city.')
+@tool(
+    "get_weather",
+    return_direct=False,
+    description="Get the current weather for a city."
+)
 def get_weather(city: str):
-    response = requests.get(f'https://wttr.in/{city}?format=j1')
+    response = requests.get(
+        f"https://wttr.in/{city}?format=j1"
+    )
+
     return response.json()
+
+
+@tool(
+    "soma",
+    return_direct=False,
+    description="Sum two numbers."
+)
+def soma(a: int, b: int):
+    return a + b
 
 
 llm = ChatOllama(
@@ -17,13 +32,22 @@ llm = ChatOllama(
     temperature=0,
     reasoning=False
 )
-llm_with_tools = llm.bind_tools([get_weather])
 
-response = llm_with_tools.invoke(
-    "What is the weather like in New York City?"
+
+agent = create_agent(
+    model=llm,
+    tools=[get_weather, soma],
 )
 
+
+response = agent.invoke({
+    "messages": [
+        {
+            "role": "user",
+            "content": "2 + 2 equal?"
+        }
+    ]
+})
+
+
 print(response)
-print("Content:", response.content)
-print("----------------------------------")
-print("Tool calls:", response.tool_calls)
